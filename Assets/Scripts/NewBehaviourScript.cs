ab02b905c68142992e8e86962037c9203ee9938b
dplacid <placid.diego@gmail.com>
2018-10-23 18:30:11 -0400

    Created NewBehaviourScript to use Emptys so that objects appear and dissapear in a specific time frame. The objectGames are initiated randomnly. The original triangleSpawn Script has been modified to destroy the task at certain times and includes code to rotate objects to that they aren't simply stationary in the air.

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public Transform[] SpawnPoints; // creates list for Spawn presets
+    public float spawnTime = 1.5f; // after how long each object will be present
+    //public GameObject Cone;
+    public GameObject[] Cones; // creates list for Cones that will Spawn
+
+
+    void Start() // initates code and only runs once throughout each session
+    {
+        InvokeRepeating("SpawnCones", spawnTime, spawnTime); //starts the Spawning process
+    }
+
+    void Update(){ // it's lonely
+
+    }
+
+    void SpawnCones()
+    {
+        int spawnIndex = Random.Range(0, SpawnPoints.Length); // how many points/presets (Emptys)
+        int objectIndex = Random.Range(0, Cones.Length); // stores length variable of Cones
+        Instantiate(Cones[objectIndex], SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation); // makes copy and distributes Spawns randomonly
+    }
+
+    /*public GameObject GameObject1;
+    public Transform SpawnPoint;
+    public Vector3 Pos;
+    public Quaternion Rot;
+
+	// Update is called once per frame
+	void Update () {
+        Pos = SpawnPoint.position;
+        Rot = SpawnPoint.rotation;
+        GameObject cone = Instantiate(GameObject1, Pos, Rot) as GameObject;
+	}*/
+}
--- Assets/Scripts/triangleSpawn.cs
+++ Assets/Scripts/triangleSpawn.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class triangleSpawn : MonoBehaviour {
-    public GameObject[] cones;
-    private IEnumerator coroutine;
+    //private IEnumerator coroutine;
+    public float destroyTime = 10.0f; // when to end spawning
+    public float rotateSpeed = 100.0f; // rotating the gameObjects (Cones)
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, destroyTime); // timer commense
         //coroutine = waitToSpawn();
         //StartCoroutine(coroutine);
         //cones[0].SetActive(true);
-        cones[0].gameObject.GetComponent<Renderer>().material.SetColor("_Color",Color.red);
+        //cones[0].gameObject.GetComponent<Renderer>().material.SetColor("_Color",Color.red);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void updated(){
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed); // direction to rotate and speed/time
+    }
 
-    IEnumerator waitToSpawn(){
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+
+    /*IEnumerator waitToSpawn(){
         Debug.Log("made it");
         while (true) {
             yield return new WaitForSeconds(50f);
             print("WaitAndPrint " + Time.time);
         }
-    }
+    }*/
 }
