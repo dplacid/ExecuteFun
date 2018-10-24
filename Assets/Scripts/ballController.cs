bb3c79f70e339ed3fd0d18ba5faf9b42ce64533f
Daniel Kimmel <dannykimmel@Daniels-MacBook-Pro.local>
2018-10-24 00:34:55 -0400

    adds shooting functionality

---
+++ Assets/Scripts/ballController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour {
+    public float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(new Vector3(h, v, 0f));
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 1000f);
+		
+	}
+}
---
+++ Assets/Scripts/ballShooterScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballShooterScript : MonoBehaviour {
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        Rigidbody shot = Instantiate(ball.GetComponent<Rigidbody>(), ball.transform.position, ball.transform.rotation) as Rigidbody;
+        shot.AddForce(ball.transform.forward * 1000f);
+    }
+}
