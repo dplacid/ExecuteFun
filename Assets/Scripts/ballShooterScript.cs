3660e09278373ef9880aa8446353b8da3790a9f4
Daniel Kimmel <dannykimmel@Daniels-MacBook-Pro.local>
2018-11-01 17:37:45 -0400

    adds button to fire

--- Assets/Scripts/ballShooterScript.cs
+++ Assets/Scripts/ballShooterScript.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ballShooterScript : MonoBehaviour {
     public GameObject ball;
+    public Button fireButton;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        fireButton.onClick.AddListener(()=> shoot());
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    private void OnMouseDown()
+    void shoot()
     {
         Rigidbody shot = Instantiate(ball.GetComponent<Rigidbody>(), ball.transform.position, ball.transform.rotation) as Rigidbody;
         shot.AddForce(ball.transform.forward * 1000f);
