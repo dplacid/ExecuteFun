18c648f79e66b54824ada46b3feb6f911fc3f9e5
Daniel Kimmel <dannykimmel@Daniels-MacBook-Pro.local>
2018-11-01 21:50:08 -0400

    changes triangle color

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -6,10 +6,12 @@ public class NewBehaviourScript : MonoBehaviour {
     public float spawnTime = 1.5f; // after how long each object will be present
     //public GameObject Cone;
     public GameObject[] Cones; // creates list for Cones that will Spawn
+    private Color[] colors;
 
 
     void Start() // initates code and only runs once throughout each session
     {
+        colors = new Color[] { Color.blue, Color.green, Color.red, Color.yellow};
         InvokeRepeating("SpawnCones", spawnTime, spawnTime); //starts the Spawning process
     }
 
@@ -19,9 +21,11 @@ public class NewBehaviourScript : MonoBehaviour {
 
     void SpawnCones()
     {
+        int color = color = Random.Range(0, 4);
         int spawnIndex = Random.Range(0, SpawnPoints.Length); // how many points/presets (Emptys)
         int objectIndex = Random.Range(0, Cones.Length); // stores length variable of Cones
         Instantiate(Cones[objectIndex], SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation); // makes copy and distributes Spawns randomonly
+        Cones[objectIndex].gameObject.GetComponent<Renderer>().sharedMaterial.SetColor("_Color",colors[color]);
     }
 
     /*public GameObject GameObject1;
--- Assets/Scripts/ballController.cs
+++ Assets/Scripts/ballController.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ballController : MonoBehaviour {
-    public float moveSpeed = 10f;
+	public Text text; 
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "cone"){
+			Renderer rend = col.gameObject.GetComponent<Renderer>();
+			if(rend.material.color == gameObject.GetComponent<Renderer>().material.color){
+				
+			}
+			Destroy(col.gameObject);
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
-        transform.Translate(new Vector3(h, v, 0f));
-        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 1000f);
 		
 	}
 }
